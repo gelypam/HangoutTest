2f7078d703b83819fd9d9c017b8847dc4bf6bbe1
Gely <gelytorresh@gmail.com>
2013-01-07 12:46:58 -0600

    ss

--- Sopa/js/hangout_demo.js
+++ Sopa/js/hangout_demo.js
@@ -203,7 +203,7 @@ function prepareViewDOM() {
 
 function onStateChange(eventObj) {
   console.log(gapi.hangout.data.getValue(SELECCIONADO));  
-  validaPalabra(SELECCIONADO);
+  $("#" + gapi.hangout.data.getValue(SELECCIONADO)).addClass("Usado");
   
 };
 
