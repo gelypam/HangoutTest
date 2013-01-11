85c7d07bf323243eaea728b7cb1acff4133b0f35
gelypam <yo_gely@hotmail.com>
2013-01-11 01:16:40 -0600

    Update Sopa/js/funcionesSopa4.js

--- Sopa/js/funcionesSopa4.js
+++ Sopa/js/funcionesSopa4.js
@@ -95,7 +95,7 @@ function startApp(){
 		//*****************************************************+//
 
 		//Comenta esta función si sólo quieres ver las 10 palabras que están dentro de la sopa de letras
-		//rellena(sopa);
+		rellena(sopa);
 		dibujaSopa(sopa);	
 		
 
